ce821fc12bff247cf3f8f31dfe51ac9cc3ca8c72
alex <alex@cloudware.it>
2014-10-13 10:40:54 +0100

    Correct URLs for pulling updates from standalone go-tictactoe app

--- update_tictactoe.sh
+++ update_tictactoe.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-BASE_URL=https://raw.github.com/crhym3/go-tictactoe/master
+BASE_URL=https://raw.githubusercontent.com/crhym3/go-tictactoe/master
 
-curl "$BASE_URL/static/images/caution.gif" > static/images/caution.gif
-curl "$BASE_URL/static/css/base.css" > static/css/tictactoe.css
-curl "$BASE_URL/static/js/base.js" > static/js/tictactoe.js
-curl "$BASE_URL/templates/tictactoe.html" > templates/tictactoe.html
+curl -sS "$BASE_URL/app/static/images/caution.gif" > static/images/caution.gif
+curl -sS "$BASE_URL/app/static/css/base.css" > static/css/tictactoe.css
+curl -sS "$BASE_URL/app/static/js/base.js" > static/js/tictactoe_base.js
+curl -sS "$BASE_URL/app/static/js/render.js" > static/js/tictactoe_render.js
+curl -sS "$BASE_URL/app/static/tictactoe.html" > templates/tictactoe.html
 
-sed -i .bak -e 's/\/js\/base\.js/\/js\/tictactoe.js/g' templates/tictactoe.html
+sed -i .bak -e 's/\/js\/base\.js/\/js\/tictactoe_base.js/g' templates/tictactoe.html
+sed -i .bak -e 's/\/js\/render\.js/\/js\/tictactoe_render.js/g' templates/tictactoe.html
 sed -i .bak -e 's/\/css\/base\.css/\/css\/tictactoe.css/g' templates/tictactoe.html
 rm -f templates/tictactoe.html.bak
